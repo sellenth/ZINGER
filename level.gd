3b9e547c699232180b906e88b9e893d0eb4c678e
Halston Sellentin <hsellentin@hygiena.com>
2024-08-31 17:13:45 -0700

    74

--- level.gd
+++ level.gd
@@ -9,7 +9,7 @@ extends Node2D
 
 # Spike trap scene to instance
 @export var spike_scene: PackedScene
-var level: int = 41
+var level: int = 72
 @export var easy_mode = false
 var rng = RandomNumberGenerator.new()
 
